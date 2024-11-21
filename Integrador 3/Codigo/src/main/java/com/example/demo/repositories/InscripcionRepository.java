e7a2d19e5b84a6918d4d4eda7fd433307792827d
matiasCS07 <mcresposchmidt@alumnos.exa.unicen.edu.ar>
2024-11-20 22:45:39 -0300

    Delete Integrador 3 directory

--- Integrador 3/Codigo/src/main/java/com/example/demo/repositories/InscripcionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entidades.Inscripcion;
-
-public interface InscripcionRepository extends JpaRepository<Inscripcion, Integer>{
-
-}
